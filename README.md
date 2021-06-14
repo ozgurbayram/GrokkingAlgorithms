1a4caf8f6ed302161680ed6dc49811993218cff4
ozgurbayram <ozgurbayram@protonmail.com>
2021-06-15 01:17:47 +0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-arr = range(50)
+## CHAPTER 1
+## I will learn fundemental algorithms such as binary search,buuble short etc.
+## Then i will learn BigO notation to analyze speed of algorithms
 
-print(arr[-3:])
+## CHAPTER 2 
+## In this chapter i will learn most used data structers arrays and linked lists
+
+## CHAPTER 3
+## This chapter will cover recursion technique that can be useful for many algorithm
+
+## Rest chapters will cover specific algorithms and problem solving techniques 
+
+
--- chapter-one/BinarySearch.py
+++ chapter-one/BinarySearch.py
@@ -1,8 +1,11 @@
 import time
 ## Binary Searhch
 def binarySearch(a,i):
+	# Low and High index's of array
 	low = 0 
 	high = len(a)-1
+	
+	# Counter
 	count = 0
 
 	while low<=high:
@@ -12,15 +15,17 @@ def binarySearch(a,i):
 			return f'Found at {mid} after {count} calculation '
 		if guess >i:
 			high = mid-1
+			# Theses 3 line of code just for visualization
 			time.sleep(1)
 			print(arr[low:high])
 			count +=1
 		else:
 			low = mid +1
+			# Theses 3 line of code just for visualization
 			time.sleep(1)
-
 			print(arr[low:high])
 			count +=1
+
 	return None
 			
 if __name__ == '__main__':
